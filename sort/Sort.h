b119662d0de859eb5b0dcc818416f496c1e2ef16
kkw <kkw@gimgeon-uui-MacBook-Air.local>
2016-05-23 01:18:32 +0900

    add Sort(selection, insertion, bubble)

--- sort/Sort.h
+++ sort/Sort.h
@@ -1,8 +1,56 @@
 #ifndef __SORT_H_
 #define __SORT_H_
+#include <stdio.h>
 
-class Sort{
-
-
+namespace Sort{
+	template <class T>
+	static void swap(T& a, T& b){
+		T tmp = a;a = b;b = tmp;
+	};
+	template <class T> static void 
+	selectionSort(T* array, int size ){
+		T min;
+		int index;
+		for(int i=0;i<size;i++){
+			min = array[i];
+			index = i;
+			for(int j=i; j<size;j++){
+				if(min > array[j]){
+					index = j;
+					min = array[j];
+				}
+			}
+			swap(array[i], array[index]);
+		}
+	};
+	template <class T> static void 
+	insertionSort(T* array, int size){
+		for(int i = 1; i<size;i++){
+			int j = i;
+			do{	
+				if( array[j] < array[j-1])
+					swap(array[j], array[j-1]);
+				else
+					break;
+			}while(--j);
+		}
+	};
+	template <class T> static void 
+	bubbleSort(T* array, int size){
+		for(int i =0;i <size-1;i++){
+			for(int j=0; j<size-1 -i;j++){
+				if( array[j] > array[j+1]){
+					swap(array[j], array[j+1]);
+				}
+			}
+		}
+	};
+	/*
+	static bool mergeSort(){};
+	static bool queckSort(){};
+	static bool heapSort(){};
+	static bool shellSort(){};
+	static bool radixSort(){};
+	*/
 };
 #endif
--- sort/main.cpp
+++ sort/main.cpp
@@ -2,10 +2,79 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "Sort.h"
 
-int main(int argc,char** argv){
+const int size = 10000;
+int* array;
 
-	printf("Test\n");
+#define RANDOM	(0)
+#define SORTED	(1)
+#define REVERSE (2)
+
+#define SELECTION_SORT 	(0)
+#define INSERTION_SORT 	(1)
+#define BUBBLE_SORT		(2)
+
+void generateArray(int array_mode){
+	array = new int[size];
+
+	for(int i =0; i< size;i++){
+		if(array_mode == RANDOM)
+			array[i] = random();
+		else if(array_mode == SORTED)
+			array[i] = i;
+		else if(array_mode == REVERSE)
+			array[size - i] = i;
+	}
+}
+bool checkSort(){
+	for(int i = 0; i<size-1;i++){
+		if( array[i] > array[i+1]){
+			printf("%d %d %d\n",i,array[i],array[i+1]);
+			return false;
+		}
+	}
+	return true;
+}
+void sortArray(int mode,int array_mode){
+	generateArray(array_mode);
+	clock_t stime = clock();
+	switch(mode){
+		case SELECTION_SORT:
+			Sort::selectionSort(array,size);
+		break;
+		case INSERTION_SORT:
+			Sort::insertionSort(array,size);
+		break;
+		case BUBBLE_SORT:
+			Sort::bubbleSort(array,size);
+		break;
+	}
+	clock_t etime = clock();
+	
+	if(!checkSort())
+		printf("-------Sort Fail\n");
+	else
+		printf("%f]\n",difftime(etime,stime));
+	delete [] array;
+}
+
+void test(int mode){
+	printf("RANDOM[");
+	sortArray(mode, RANDOM);
+	printf("SORTED[");
+	sortArray(mode, SORTED);
+	printf("REVERSE[");
+	sortArray(mode, REVERSE);
+}
+
+int main(int argc,char** argv){
+	printf("\nSELECTION_SORT\n");
+	test(SELECTION_SORT);
+	printf("\nINSERTION_SORT\n");
+	test(INSERTION_SORT);
+	printf("\nBUBBLE_SORT\n");
+	test(BUBBLE_SORT);
 	return 0;
 }
